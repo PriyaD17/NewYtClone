e6695a93d2aa46f9ff1e1dcb7be7c156bb46e875
Priya <ganapathipriyadharsani@gmail.com>
2024-11-29 22:46:04 +0530

    Added Video Card Component

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import {VideoCard} from "@/components/VideoCard"
+
+export default function Home() {
+  return (
+    <div>
+      <VideoCard 
+      title={"this poor girl"}
+      ctmb= {"/cp.jpg"}
+      author={"Gangsta Perspectives"}
+      views={"10k"}
+      time={"1 hour"}
+
+      > </VideoCard>
+    </div>
+  );
+}
